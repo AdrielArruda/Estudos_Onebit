8b4454ac422fb1fd231a736b3aaedb0c705b448d
AdrielArruda <103084621+AdrielArruda@users.noreply.github.com>
2022-04-27 05:16:29 -0300

    adicionado exercicios de javascript

---
+++ ex017 - exercicio utilizando javascript externo/diferenca-idade.js
@@ -0,0 +1,11 @@
+let nameVelha = prompt("Qual o nome da pessoas mais velha?")
+
+let ageVelha = prompt("Qual a idade da pessoa mais velha?")
+
+let pessoaNova = prompt("Qual o nome da pessoa mais nova?")
+
+let ageNova = prompt("Qual a idade da pessoa mais nova?")
+
+let idadeDiferent = ageVelha - ageNova
+
+alert("A pessoas mais velha é " + nameVelha + " com " + ageVelha + " anos" + "\n\nA pessoas mais nova é " + pessoaNova + " com " + ageNova + " anos" + "\n\nA diferença de idade entre eles é de: " + idadeDiferent + " anos")
